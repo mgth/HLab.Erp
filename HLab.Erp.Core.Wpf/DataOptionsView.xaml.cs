c845ce0d18790c1b6d4a2edced562f87b6356f4a
mgthieu <mathieu@lesbouys.net>
2019-11-03 10:38:23 +0100

    data options

---
+++ HLab.Erp.Core.Wpf/DataOptionsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HLab.Erp.Core
+{
+    /// <summary>
+    /// Logique d'interaction pour DataOptionsView.xaml
+    /// </summary>
+    public partial class DataOptionsView : UserControl
+    {
+        public DataOptionsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
