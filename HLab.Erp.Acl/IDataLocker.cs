bf6bf68628270077d92a29d8474ad9e55fe67218
mathieu <mathieu@lesbouys.net>
2020-01-09 16:12:25 +0100

    New datalocker commands

--- HLab.Erp.Acl/IDataLocker.cs
+++ HLab.Erp.Acl/IDataLocker.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Windows.Input;
 
 namespace HLab.Erp.Acl
 {
     public interface IDataLocker : IDisposable
     {
         bool IsActive { get; }
+        ICommand ActivateCommand { get; }
+        ICommand SaveCommand { get; }
+        ICommand CancelCommand { get; }
     }
 }
