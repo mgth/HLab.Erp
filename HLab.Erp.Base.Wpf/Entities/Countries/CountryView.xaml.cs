7c2aa79af9fa158c159afab837044827038d9d48
mathieu <mathieu@lesbouys.net>
2020-01-09 13:53:40 +0100

    Deal with countries

---
+++ HLab.Erp.Base.Wpf/Entities/Countries/CountryView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HLab.Erp.Core;
+using HLab.Mvvm.Annotations;
+
+namespace HLab.Erp.Base.Wpf.Entities.Countries
+{
+    /// <summary>
+    /// Logique d'interaction pour CountryView.xaml
+    /// </summary>
+    public partial class CountryView : UserControl, IView<CountryViewModel>, IViewClassDocument
+    {
+        public CountryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HLab.Erp.Base.Wpf/Entities/Countries/CountryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HLab.Erp.Acl;
+using HLab.Erp.Base.Data;
+using HLab.Notify.PropertyChanged;
+
+namespace HLab.Erp.Base.Wpf.Entities.Countries
+{
+    public class CountryViewModel : EntityViewModel<CountryViewModel,Country>
+    {
+        public string Title => _title.Get();
+
+        private readonly IProperty<string> _title = H.Property<string>(c => c
+            .On(e => e.Model.Name)
+            .Set(e => "{Country} - " + e.Model.Name)
+        );
+    }
+}
