160086060f7b34849f2e3b1ef2cac305e3e0b58c
mathieu <mathieu@lesbouys.net>
2020-01-09 16:08:47 +0100

    extract corporation from customers

---
+++ HLab.Erp.Base.Data/ICorporation.cs
@@ -0,0 +1,15 @@
+namespace HLab.Erp.Base.Data
+{
+    public interface ICorporation
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string Phone { get; set; }
+        string Fax { get; set; }
+        string Email { get; set; }
+        string Note { get; set; }
+
+        int? CountryId { get; set; }
+        Country Country { get; set; }
+    }
+}
