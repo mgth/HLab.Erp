81a5c7f2134e4510ed14d6e1e8dca4af42c63b10
mgthieu <mathieu@lesbouys.net>
2020-03-12 08:35:41 +0100

    Erp specific rights

---
+++ HLab.Erp.Base.Wpf/ErpRights.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HLab.Erp.Acl;
+
+namespace HLab.Erp.Base.Wpf
+{
+    public static class ErpRights
+    {
+        /// <summary>
+        /// Allow to create / Delete users
+        /// </summary>        
+        public static readonly AclRight SignCustomer = AclRight.Get();
+    }
+}
