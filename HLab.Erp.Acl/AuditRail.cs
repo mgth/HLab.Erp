ddeb219b72af2dada009ca8cf7a89ece948e50c6
mathieu <mathieu@lesbouys.net>
2020-04-16 12:06:29 +0200

    set readonly properties

--- HLab.Erp.Acl/AuditRail.cs
+++ HLab.Erp.Acl/AuditRail.cs
@@ -13,57 +13,57 @@ namespace HLab.Erp.Acl
             get => _entityClass.Get();
             set => _entityClass.Set(value);    
         }
-        private IProperty<string> _entityClass = H.Property<string>();
+        private readonly IProperty<string> _entityClass = H.Property<string>();
         public string EntityCaption
         { 
             get => _entityCaption.Get();
             set => _entityCaption.Set(value);    
         }
-        private IProperty<string> _entityCaption = H.Property<string>();
+        private readonly IProperty<string> _entityCaption = H.Property<string>();
         public string UserCaption 
         { 
             get => _userCaption.Get();
             set => _userCaption.Set(value);    
         }
-        private IProperty<string> _userCaption = H.Property<string>();
+        private readonly IProperty<string> _userCaption = H.Property<string>();
 
         public int? EntityId
         { 
             get => _entityId.Get();
             set => _entityId.Set(value);    
         }
-        private IProperty<int?> _entityId = H.Property<int?>();
+        private readonly IProperty<int?> _entityId = H.Property<int?>();
         public DateTime TimeStamp
         { 
             get => _timeStamp.Get();
             set => _timeStamp.Set(value);    
         }
-        private IProperty<DateTime> _timeStamp = H.Property<DateTime>();
+        private readonly IProperty<DateTime> _timeStamp = H.Property<DateTime>();
         public string Motivation 
         { 
             get => _motivation.Get();
             set => _motivation.Set(value);    
         }
-        private IProperty<string> _motivation = H.Property<string>();
+        private readonly IProperty<string> _motivation = H.Property<string>();
         public string Log 
         { 
             get => _log.Get();
             set => _log.Set(value);    
         }
-        private IProperty<string> _log = H.Property<string>();
+        private readonly IProperty<string> _log = H.Property<string>();
         public string IconPath  
         { 
             get => _iconPath.Get();
             set => _iconPath.Set(value);    
         }
-        private IProperty<string> _iconPath = H.Property<string>();
+        private readonly IProperty<string> _iconPath = H.Property<string>();
 
         public string Action  
         { 
             get => _action.Get();
             set => _action.Set(value);    
         }
-        private IProperty<string> _action = H.Property<string>();
+        private readonly IProperty<string> _action = H.Property<string>();
 
         public int? UserId
         { 
@@ -77,14 +77,14 @@ namespace HLab.Erp.Acl
             get => _user.Get();
             set => _user.Set(value);    
         }
-        private IForeign<User> _user = H.Foreign<User>();
+        private readonly IForeign<User> _user = H.Foreign<User>();
         public bool? Failed 
         { 
             get => _failed.Get();
             set => _failed.Set(value);    
         }
 
-        private IProperty<bool?> _failed = H.Property<bool?>();
+        private readonly IProperty<bool?> _failed = H.Property<bool?>();
 
     }
 }
