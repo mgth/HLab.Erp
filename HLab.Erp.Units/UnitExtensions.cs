f5f010a8ca670e0d91066a8081adca2c971a9bc4
mgth <mathieu@mgth.fr>
2021-06-07 17:55:19 +0200

    rearange

---
+++ HLab.Erp.Units/UnitExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace HLab.Erp.Units
+{
+    public static class UnitExtensions
+    {
+        public static Unit BestMatch(this IEnumerable<Unit> units, double absQty)
+        {
+            Unit bestUnit = null;
+            double bestQty = 0;
+            foreach (var unit in units)
+            {
+                var qty = unit.Qty(absQty);
+
+                if (bestUnit == null || (qty < bestQty && qty >= 1) || (bestQty < 1 && qty > bestQty))
+                {
+                    bestUnit = unit;
+                    bestQty = qty;
+                }
+            }
+            return bestUnit;
+
+        }
+    }
+}
---
+++ HLab.Erp.Units/UnitService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HLab.Erp.Data;
+using HLab.Erp.Data.Observables;
+
+namespace HLab.Erp.Units
+{
+    public interface IUnitService
+    {
+            IObservableQuery<Unit> Units { get; }
+            IEnumerable<Unit> GetGroup(string group);
+
+            Unit Default(string group);
+    }
+
+    public class UnitService : IUnitService
+    {
+        private IDataService _db;
+
+        public void Inject(IDataService db, IObservableQuery<Unit> units)
+        {
+            _db = db;
+            Units = units;
+        }
+
+        public IObservableQuery<Unit> Units { get; private set;}
+
+        public IEnumerable<Unit> GetGroup(string group)
+        {
+            return Units.Where(u => u.Group == group);
+        }
+
+        public Unit Default(string @group)
+        {
+            return Units.FirstOrDefault(u => u.Group == group && u.Default);
+        }
+    }
+}
